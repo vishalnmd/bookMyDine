7f8cb9472568e14e4d60f77a47c59f1d2cae702c
vishalnmd <vishalnmd.vishalkhairagarh@gmail.com>
2026-05-07 01:45:26 +0530

    initialize project

---
+++ BookMyDine/src/main/java/com/bookmydine/BookMyDineApplication.java
@@ -0,0 +1,13 @@
+package com.bookmydine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyDineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyDineApplication.class, args);
+	}
+
+}
---
+++ BookMyDine/src/test/java/com/bookmydine/BookMyDineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmydine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyDineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
