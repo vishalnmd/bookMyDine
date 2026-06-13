7a9f27047a9b6b52a9b9f000e12ef0ae9c949cdb
Vishal <76469664+vishalnmd@users.noreply.github.com>
2026-06-14 03:56:05 +0530

    added resturant entity (#23)
    Co-authored-by: vishalnmd <vishalnmd.vishalkhairagarh@gmail.com>

---
+++ BookMyDine/src/main/java/com/bookmydine/common/enums/FoodType.java
@@ -0,0 +1,6 @@
+package com.bookmydine.common.enums;
+
+public enum FoodType {
+    VEG,
+    NON_VEG
+}
---
+++ BookMyDine/src/main/java/com/bookmydine/restaurant/entity/Restaurant.java
@@ -0,0 +1,28 @@
+package com.bookmydine.restaurant.entity;
+
+import com.bookmydine.common.entity.BaseEntity;
+import com.bookmydine.common.enums.FoodType;
+import com.bookmydine.user.entity.User;
+import jakarta.persistence.*;
+
+@Entity
+public class Restaurant extends BaseEntity {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private FoodType foodType;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")  // ← FK in ORDER table
+    private User user;
+
+}
